430236a66d7285f6706ae007843298c653aeeb5f
Krubersreik-5 <voidofdarkness777@gmail.com>
2024-10-31 14:52:58 -0700

    Main menu and game over screens and menu script

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*
+ * Benjamnin Smith
+ * 10/28/2024
+ * Changes the scene to the game or quits the game
+ */
+public class Menu : MonoBehaviour
+{
+    public GameObject playText;
+    public GameObject quitText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void Quit()
+    {
+        print("Quit Game");
+        Application.Quit();
+    }
+}
