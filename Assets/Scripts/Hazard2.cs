059134c04378a578a3e1b0530400bf144fe1bbf6
mdparizeau <6michaels@gmail.com>
2024-11-05 14:43:55 -0800

    description and add health to enemies

--- Assets/Scripts/Hazard2.cs
+++ Assets/Scripts/Hazard2.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Michael Parizeau
+/// 10/31/24
+/// Hazard script to attach to Hard Enemies
+/// <summary>
 public class Hazard2 : MonoBehaviour
 {
+    public int health = 10;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<Player>())
