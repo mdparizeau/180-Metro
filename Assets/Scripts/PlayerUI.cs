64047820bd05ffb8323bae657b1a06be645b9bfd
mdparizeau <6michaels@gmail.com>
2024-11-05 14:54:33 -0800

    adjusted hp ui

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+/// <summary>
+/// Cetz, Zulema
+/// 11/4/24
+/// Script for updating UI of Player's Health
+/// </summary>
 public class PlayerUI : MonoBehaviour
 {
     public Player player;
-
     public TMP_Text hpText;
 
     private void Update()
@@ -15,10 +19,4 @@ public class PlayerUI : MonoBehaviour
         hpText.text = "HP: " + player.health;
 
     }
-
-
-
-
-
-
 }
