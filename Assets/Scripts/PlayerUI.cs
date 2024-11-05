2da16355ffc7024454f2d06c2f4394277eea1f48
zulema <zcetz@comcast.net>
2024-11-04 16:48:14 -0800

    UI

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class PlayeUI : MonoBehaviour
+public class PlayerUI : MonoBehaviour
 {
     public Player player;
+
     public TMP_Text hpText;
 
     private void Update()
     {
         hpText.text = "HP: " + player.health;
+
     }
+
+
+
+
+
+
 }
