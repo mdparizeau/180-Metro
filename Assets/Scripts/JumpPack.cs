605c3c122ce4c0f1af9be11542d438ff86a57aa4
zulema <zcetz@comcast.net>
2024-11-12 15:19:16 -0800

    jumppack respawning (notworkingyet)

--- Assets/Scripts/JumpPack.cs
+++ Assets/Scripts/JumpPack.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class JumpPack : MonoBehaviour
 {
-  
+    public GameObject jumpPack;
+    private Vector3 packPosition;
 
+    void OnTriggerEnter(Collider other)
+    {
+
+        StartCoroutine("RespawnPack");
+
+    }
+
+    IEnumerator RespawnPack()
+    {
+
+        GameObject clone = (GameObject)Instantiate(jumpPack, packPosition, Quaternion.identity) as GameObject;
+
+        Destroy(jumpPack);
+
+        yield return null;
+
+    }
 }
