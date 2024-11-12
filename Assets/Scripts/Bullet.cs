9f168ec770eeafca89fd652625e4bd756583e5a3
mdparizeau <6michaels@gmail.com>
2024-11-12 15:22:13 -0800

    bullet cleanup

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,21 +9,10 @@ using UnityEngine;
  */
 public class Bullet : MonoBehaviour
 {
-    private Player playerScript;
+    public Player playerScript;
+    //public Material mat;
     public bool goingLeft;
-    //float raycastDist = 0.5f;
     public float speed;
-    private Vector3 leftCheck;
-    private Vector3 rightCheck;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerScript = GetComponent<Player>();
-        /*float halfWidth = (transform.localScale.x / 2) + 0.1f;
-        rightCheck = transform.position + new Vector3(halfWidth, 0, 0);
-        leftCheck = transform.position - new Vector3(halfWidth, 0, 0);*/
-    }
 
     /// <summary>
     /// Makes the bullet move left or right based on whether the goingLeft bool is true or false
@@ -34,20 +23,8 @@ public class Bullet : MonoBehaviour
         if (goingLeft == true)
             transform.position += speed * Vector3.left * Time.deltaTime;
         else transform.position += speed * Vector3.right * Time.deltaTime;
-
-        /*RaycastHit hit;
-        if (Physics.Raycast(rightCheck, Vector3.right, out hit)
-            || Physics.Raycast(leftCheck, Vector3.left, out hit))
-        {
-            if (!hit.collider.GetComponent<Shooting>() && !gameObject.GetComponent<Bullet>())
-            {
-                this.gameObject.SetActive(false);
-            }
-                //this.gameObject.SetActive(false);
-
-        }
-        Debug.DrawRay(transform.position, Vector3.left * raycastDist, Color.red);
-        Debug.DrawRay(transform.position, Vector3.right * raycastDist, Color.red);*/
+        /*if (playerScript.HB)
+            this.GetComponent<Renderer>().material = mat;*/
     }
     private void OnTriggerEnter(Collider other)
     {
